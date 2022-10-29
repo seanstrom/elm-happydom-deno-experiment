424bac35c49961e5e95f38a2322ae48738a29cc6
Sean Hagstrom <sean@seanhagstrom.com>
2022-10-29 16:15:02 +0100

    disable sourcemaps and optimize in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,19 @@ import elm from './utils/elm-rollup-plugin.js';
 
 
 const optimize = false;
+const sourcemap = false;
 
 
 const elmClientConfig = {
     input: 'src/client.elm.js',
     output: {
-        sourcemap: true,
+        sourcemap,
         file: 'dist/client.elm.js',
         format: 'es',
         exports: 'auto',
     },
     plugins: [
-        elm({ compiler: { optimize: false }}),
+        elm({ compiler: { optimize } }),
     ],
 };
 
@@ -22,26 +23,26 @@ const elmClientConfig = {
 const elmServerConfig = {
     input: 'src/server.elm.js',
     output: {
-        sourcemap: true,
+        sourcemap,
         file: 'dist/server.elm.js',
         format: 'es',
         exports: 'auto',
     },
     plugins: [
-        elm({ compiler: { optimize: optimize }}),
+        elm({ compiler: { optimize } }),
     ],
 };
 
 const elmTestServerConfig = {
     input: 'src/worker.elm.js',
     output: {
-        sourcemap: true,
+        sourcemap,
         file: 'dist/worker.elm.js',
         format: 'es',
         exports: 'auto',
     },
     plugins: [
-        elm({ compiler: { optimize: optimize }}),
+        elm({ compiler: { optimize } }),
     ],
 };
 
@@ -49,13 +50,13 @@ const elmTestServerConfig = {
 const clientPlatformConfig = {
     input: 'src/client.platform.js',
     output: {
-        sourcemap: true,
+        sourcemap,
         file: 'dist/client.platform.js',
         format: 'es',
         exports: 'auto',
     },
     plugins: [
-        elm({ compiler: { optimize: optimize }}),
+        elm({ compiler: { optimize } }),
         alias({
             entries: [
                 { find: '@src/client.elm.js', replacement: './src/client.elm.js' }
